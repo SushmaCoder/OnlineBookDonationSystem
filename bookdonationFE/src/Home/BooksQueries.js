7a29afe706c60aa5f0a51e98eb0a7be62234cd57
SushmaCoder <Sushusterling@gmail.com>
2021-02-12 17:46:18 +0530

    added screenshots

--- bookdonationFE/src/Home/BooksQueries.js
+++ bookdonationFE/src/Home/BooksQueries.js
@@ -7,8 +7,20 @@ export default function BooksQueries(){
     const {error,loading,data} = useQuery(GET_BOOKS);
    
     return error ? (<div>error</div>) : loading ? (<div>loading</div>) :(
+        <div>
+    {/* <div className="flex  justify-between items-center bg-dark-gray pa3">
+      <div className="white f3-ns align-center-ns mv2-ns w-30 ml5">Book Donation Site</div>
+      </div> */}
+      <div className="flex  justify-center items-center bg-gold relative">
+      <div className="bg-mid-gray o-90 pa5 w-90 mv4">
+      <div className="flex  justify-center items-center white f2 ">List of the books</div>
+      </div>
+      </div>
+      <div className="flex justify-center items-center ma4 ml5">
+     <button className="pa2 w4 bg-light-gray" onClick={() =>window.history.back()}> Back</button>
+     </div>
          <div className="flex flex-column justify-between items-center">
-         <div className="f2">List of the books</div>
+         <div className="f2"></div>
         {/* <table class="f6 miw ba b--black-20" cellspacing="0">
          <tr className="">
          <th className="th"  colspan="1">hello</th>
@@ -33,6 +45,7 @@ export default function BooksQueries(){
          </tr> )}
      </table> 
      </div>
+    </div>
     </div>
     )
 }
--- bookdonationFE/src/Home/Home.js
+++ bookdonationFE/src/Home/Home.js
@@ -3,39 +3,63 @@ import {useEffect,useState}  from 'react'
 import {useMutation} from '@apollo/react-hooks';
 import {UPDATE_BOOKS} from '../Api/queries';
 import { navigate } from '@reach/router';
-import hello from '../Assets/bookss.png'
+// import hello from '../Assets/bookss.png'
 import bklogo from '../Assets/booklogo.png'
 
 
 export default function Home(){
-     const [msgBox, setmsgBox] = useState(true);
+     const [msg, setMsg] = useState("");
      const [title, setTitle] = useState('');
      const [authr,setAuthr] = useState('');
      const [ISBN,setISBN] = useState('');
-     const [errBox, setErrBox] = useState('');
+     const [check, setCheck] = useState(true);
+     const [err,setErr]=useState(false);
      const [errMsg,seterrMsg] = useState('');
     //  const { loading, error, data } = useQuery(EXCHANGE_RATES);
      const [updatethebooks,{currentstatus}] = useMutation(UPDATE_BOOKS);
     
 
     console.log("currentstatus",currentstatus);
+    function setDefaultValue () {
+      console.log("i am called")
+      setTitle('')
+      setAuthr('')
+      setISBN('')
+      console.log(title)
+    }
     const getBooksInfo = async (title,author,ISBN) => {
       // console.log("parseInt(title)",parseInt(title));
       // console.log("typeof(title)",typeof(parseInt(title)));
-      if(isNaN(title)===true){
-        console.log("Alphanumeric")
-      }else{
-        console.log("pure number")
-      }
+      // if(isNaN(title)===true){
+      //   console.log("Alphanumeric")
+      // }else{
+      //   console.log("pure number")
+      // }
+      if(title!="" && author!="" && ISBN!=""){
+        document.getElementById("bk").value="";
+        document.getElementById("Au").value="";
+        document.getElementById("isbn").value="";
       await updatethebooks({variables:{bookname:title,author:author,ISBN:ISBN}});
-    };
-    var d = (new Date()).toString().split(' ').splice(1,3).join(' ');
-    const errmsg = (a) => {
-      if(a==true){
-        return true;
+      setDefaultValue();
       }
-        return false;
-    }
+    };
+    const timeout = async() => {
+    setTimeout(() => {
+      setErr(false);
+  }, 2600);
+}
+const working = async() => {
+  setTimeout(() => {
+    setMsg("");
+}, 800);
+}
+    var d = (new Date()).toString().split(' ').splice(1,3).join(' ');
+    // const errmsg = (a) => {
+    //   if(a==true){
+    //     return true;
+    //   }
+    //     return false;
+    // }
 
   
 
@@ -61,45 +85,69 @@ export default function Home(){
       <div className="flex  justify-center items-center bg-gold relative">
       {/* <img className="mv3" src={hello}/> */}
       <div className="bg-mid-gray o-90 pa5 w-90 mv4">
-      <div className="white f2">Welcome to Books Donation online Site</div>
-      <div className="flex  justify-between items-center pt2 absolute">
+      <div className="flex  justify-center items-center white f2 ">Welcome to Books Donation online Site</div>
+      {/* <div className="flex  justify-between items-center pt2 absolute">
       <div className="white f2 mr3">By SushmaC</div>
       <div className="white f2 mr3">JavaScript</div>
       <div className="white f2">{d}</div>
-      </div>
+      </div> */}
       </div>
       </div>
       <div className="flex flex-column">
-      <div className="flex justify-center items-center mv2">
-      <div className="f3">Enter the Book name : </div>
+      <div className="flex justify-center items-center mv2 mr2">
+      <div className="f3 pr2">Enter the Book name : </div>
      {/* <input id="name" className="border-input  ba b--black-05" placeholder="bookname" type="text"></input> */}
-     <input  className="pa2 w5 ba b--gray-10" placeholder="Bookname" onChange={event => setTitle(event.target.value)} />
+     <input  className="pa2 w5  b--gray-10" placeholder="Bookname" id="bk" onChange={event => setTitle(event.target.value)} />
      </div>
-      <div className="flex justify-center items-center mv2">
-      <div className="f3">Enter the Author name:</div>
+      <div className="flex justify-center items-center mv2 ml2">
+      <div className="f3 pr2">Enter the Author name : </div>
       {/* <input id="name" className="border-input  ba b--black-05" placeholder="bookname" type="text" value=""></input> */}
-      <input  className="border-input  ba b--gray-10 pa2 w5" placeholder="Authorname" onChange={event => setAuthr(event.target.value)} />
+      <input  className="border-input   b--gray-10 pa2 w5" placeholder="Authorname" id="Au" onChange={event => setAuthr(event.target.value)} />
       </div>
       <div className="flex justify-center items-center mv2">
-      <div className="f3">Enter the ISBN:</div>
+      <div className="f3 pr2">Enter the book's ISBN : </div>
       {/* <input id="name" className="border-input  ba b--black-05" placeholder="bookname" type="text" value=""></input> */}
-      <input  className="border-input  ba b--gray-10 pa2 w5" placeholder="ISBN" onChange={event => setISBN(event.target.value)} />
+      <input  className="border-input b--gray-10 pa2 w5" placeholder="ISBN" id="isbn" onChange={event => setISBN(event.target.value)} />
       </div>
       </div>
       <div className="flex justify-center items-center mv4">
      {/* {(title.length==0)||(authr.length==0)||(ISBN.length==0)}?  */}
      {/* <button className="pa2 w4" onClick={() =>}>Submit</button> */}
      {/* <button className="pa2 w4">Submit</button> */}
-     {errBox &&
+     {/* {errBox &&
     <div className="flex">
         <div className="bg-gold f6 white fw1 flex justify-center  items-center pa2 w-100 pr4"><div className="ba br-50 pa2 w2 h2 white flex justify-center items-center relative"><div className="absolute">!</div></div><span style={{ marginLeft: 12 }}></span>Hey</div>
     </div>
-    } 
+    }  */}
     {/* ? */}
-    {/* {items===true? <div className="" onClick={()=> setItems(false)}>True</div>:<div className="" onClick={()=> setItems(true)}>false</div>} */}
-     <button className="pa2 w4 bg-light-blue" onClick={() => setErrBox(false),getBooksInfo(title,authr,ISBN)}>Submit</button>
-     {/* {msgBox===true? <div>Successfully added</div>:} */}
+    {/* {items===true? <div className="" onClick={()=> setItems(false)}>True</div>:<div className="" onClick={()=> setItems(true)}>false</div>} ,getBooksInfo(title,authr,ISBN)*/}
+    {/* {check === true ? <button className="pa2 w4 bg-red" onClick={(e) => { 
+      e.preventDefault();
+      setCheck(false);
+      getBooksInfo(title,authr,ISBN)}}>Sushi</button> */}
+      {/* {check === true && title==="" && authr==="" && ISBN==="" ? <button className="pa2 w4 bg-light-gray" onClick={() => { setErr(true);setCheck(false);
+      }}>Submit</button> : */}
+      {check === true && title && authr && ISBN ? <button className="pa2 w4 bg-light-blue" onClick={() => { 
+      setCheck(false);
+      setErr(false);
+      getBooksInfo(title,authr,ISBN);
+      setMsg("Succesfully inserted");
+      working();
+      }}>Submit</button>
+    : check===false && title && authr && ISBN ? <button className="pa2 w4 bg-light-blue" onClick={() => { 
+      setCheck(false);
+      setErr(false);
+      setMsg("Succesfully inserted");
+      working();
+      getBooksInfo(title,authr,ISBN);
+      }}>Submit</button> :
+      <button className="pa2 w4 bg-light-gray" onClick={() => { setErr(true);timeout();}}>Submit</button>}
+    
+   
+    {/* <div className="pa2 w5 bg-yellow">Successfully Inserted</div> */}
       </div>
+      {err && msg=="" && <div className="flex justify-center items-center mv4"><div className="pa2 w5 bg-yellow mt2 tc">please fill all the details</div></div>}
+    {msg!="" && <div className="flex justify-center items-center mv4"><div className="pa2 w5 bg-yellow mt2 tc">{msg}</div></div>}
       <footer className="bg-mid-gray o-90 ph7-l w-100 dn db-l pv4 fixed l0 b0" style={{ left: 0,bottom: 0}}>
       <div className="flex justify-between items-center">
         <div className="white">Give a new Life to your used books</div>
